8a048f603942a6c9081f22973d4eef58f29bd321
Alfian Dimas <kidaliez@gmail.com>
2019-02-16 10:13:18 +0700

    migrate from mongodb to mysql, delete

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-
-const LevelController = require('./controllers/level')
-const AdminController = require('./controllers/admin')
-const CostController = require('./controllers/cost')
-const CustomerController = require('./controllers/customer')
-const UsageController = require('./controllers/usage')
-const BillController = require('./controllers/bill')
 
 const app = express()
 
@@ -25,32 +17,6 @@ app.get('/', (req,res) => {
 
   res.json(data)
 })
-
-app.post('/level', LevelController.show)
-
-app.post('/admin', AdminController.show)
-app.post('/admin/login', AdminController.login)
-app.put('/admin/update', AdminController.update)
-app.delete('/admin/delete', AdminController.delete)
-
-app.post('/cost', CostController.show)
-app.post('/cost/add', CostController.create)
-app.put('/cost/update', CostController.update)
-app.delete('/cost/delete', CostController.delete)
-
-app.post('/customer', CustomerController.show)
-app.post('/customer/add', CustomerController.create)
-app.put('/customer/update', CustomerController.update)
-app.delete('/customer/delete', CustomerController.delete)
-
-app.post('/usage', UsageController.show)
-app.post('/usage/show', UsageController.detail)
-app.post('/usage/add', UsageController.create)
-
-app.post('/bill', BillController.show)
-
-mongoose.connect('mongodb://localhost:27017/api-ppob',{ useNewUrlParser: true })
-mongoose.Promise = global.Promise
 
 app.listen(3000, () => {
   console.log('Server listening on localhot:3000')
